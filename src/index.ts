357d56cc71238d2d431d13f179274d2e68fd18e2
Shehan <marionilanka@gmail.com>
2024-02-29 23:26:18 +0530

    first commit

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import "dotenv/config";
+import mongoose from "mongoose";
+import myUserRoute from "./routes/MyUserRoute";
+
+// Connect to MongoDB using Mongoose
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
+  .then(() => console.log("Connected to database!"))
+  .catch(error => console.error("Error connecting to database:", error));
+
+// Create an Express application
+const app = express();
+// Middleware to parse JSON bodies
+app.use(express.json());
+// Middleware to enable CORS
+app.use(cors());
+
+app.get("/health", async (req: Request, res: Response) => {
+  res.send({ message: "health OK!" });
+});
+// Define a route at /test which responds with a JSON message "Hello!"
+app.get("/test", async (req: Request, res: Response) => {
+    res.json({ message: "Hello!" });
+});
+
+// Start the server on port 7000
+const PORT = process.env.PORT || 7000;
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
+
+app.use("/api/my/user", myUserRoute);
+
